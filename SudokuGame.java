4da8c77045cfe37f6f3f91db9d2804a0c584ce52
SuchitaSriramka <suchitasriramka@gmail.com>
2022-03-05 12:52:17 +0530

    Update SudokuGame.java

--- SudokuGame.java
+++ SudokuGame.java
@@ -3,15 +3,7 @@ package sudoku;
 public class SudokuGame {
 
 	public static void main(String[] args) {
-		/*
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				SudokuFrame frame = new SudokuFrame();
-				frame.setVisible(true);
-			}
-		});
-		*/
+		
 		SudokuFrame frame = new SudokuFrame();
 		frame.setVisible(true);
 		
